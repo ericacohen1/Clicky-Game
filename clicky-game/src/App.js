b94fa3d60e3336f34ee6d704891b215612bb078f
Erica  Cohen <ericacohen@Ericas-Air.lan>
2018-02-04 16:59:14 -0800

    handle increment function

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -10,12 +10,53 @@ import Wrapper from "./components/Wrapper";
 class App extends Component {
   // Setting this.state.friends to the images json array
   state = {
-    images
+    images,
+    currentScore: 0,
+    highScore: 0,
+    clicked: [],
+    alert: ""
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState ({ clicked: this.state.clicked.concat(id)});
+    }
+    else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+   const currentGameScore = this.state.currentScore + 1;
+   this.setState({
+     currentScore: currentGameScore
+   });
+   if (currentGameScore >= this.state.highScore) {
+     this.setState({ highScore: currentGameScore });
+   }
+  //   this.setState({ currentScore: this.state.currentScore + 1 });
+  //   if (currentScore >= this.state.highScore) {
+  //     this.setState({ highScore: currentScore });
+  //   }
+    else if (currentGameScore === 12) {
+        this.setState({ alert: "YOU WON!!" });
+      }
+    }
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      highScore: this.state.highScore,
+      clicked: []
+    })
   };
 
   render() {
     return (
       <Wrapper>
+      <p>Current Score: {this.state.currentScore}</p>
+      <p>High Score: {this.state.highScore}</p>
       {this.state.images.map(images => (
       <FruitCard
       key={images.id}
